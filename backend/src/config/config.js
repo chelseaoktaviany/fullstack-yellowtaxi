6fa7a1b9347bfc03fee1d431da405437ad2f3c83
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2024-10-29 15:18:50 +0700

    Fixed security

---
+++ backend/src/config/config.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+module.exports = {
+  development: {
+    username: process.env.PSQL_USER,
+    password: String(process.env.PSQL_PASSWORD),
+    database: process.env.PSQL_DB,
+    host: String(process.env.PSQL_HOST),
+    dialect: "postgres",
+  },
+};
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,16 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: ".env" });
 
-const sequelize = new Sequelize("yellowtaxitrip_db", "postgres", "test123", {
-  logging: console.log,
-  host: "localhost",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.PSQL_DB,
+  process.env.PSQL_USER,
+  String(process.env.PSQL_Password),
+  {
+    logging: console.log,
+    host: process.env.PSQL_LOCALHOST,
+    dialect: "postgres",
+  }
+);
 
 const testConnection = async () => {
   try {
