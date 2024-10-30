379bb81393acfb890d3c345b6fd6e3366dc4ab3d
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2024-10-30 12:08:37 +0700

    Updated dev mode

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,6 +2,13 @@ const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 
 module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: String(process.env.PSQL_PASSWORD),
+    database: process.env.PSQL_DB,
+    host: String(process.env.PSQL_HOST),
+    dialect: "postgres",
+  },
   production: {
     username: process.env.DB_USER,
     password: String(process.env.PSQL_PASSWORD),
