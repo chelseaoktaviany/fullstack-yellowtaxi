71e9b7b2d57f7fc2f68fcec93f5a493eb430ff7e
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2024-10-30 14:16:26 +0700

    Updated

--- backend/src/controllers/tripController.js
+++ backend/src/controllers/tripController.js
@@ -1,59 +1,49 @@
 const { Trips } = require("../models");
 
 exports.getAllTrip = async (req, res) => {
-  try {
-    const trips = await Trips.findAll();
-    res.status(200).json({
-      isSuccess: true,
-      data: trips,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      isSuccess: false,
-      msg: "Failed to get all trips' data",
-    });
-  }
-};
-
-exports.getTrip = async (req, res) => {
   // try {
-  //   const { id } = req.params;
-
-  //   const trip = await Trips.findByPk(id);
-
-  //   if (trip) {
-  //     res.status(200).json({
-  //       isSuccess: true,
-  //       data: trip,
-  //     });
-  //   } else {
-  //     res.status(404).json({
-  //       isSuccess: false,
-  //       msg: "Trip not found",
-  //     });
-  //   }
+  //   const trips = await Trips.findAll();
+  //   res.status(200).json({
+  //     isSuccess: true,
+  //     data: trips,
+  //   });
   // } catch (err) {
   //   console.error(err);
   //   res.status(500).json({
   //     isSuccess: false,
-  //     msg: "Failed to get a trip data",
+  //     msg: "Failed to get all trips' data",
   //   });
   // }
 
-  const { id } = req.params;
+  const trips = await Trips.findAll();
+  res.status(200).json({
+    isSuccess: true,
+    data: trips,
+  });
+};
 
-  const trip = await Trips.findByPk(id);
+exports.getTrip = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-  if (trip) {
-    res.status(200).json({
-      isSuccess: true,
-      data: trip,
-    });
-  } else {
-    res.status(404).json({
+    const trip = await Trips.findByPk(id);
+
+    if (trip) {
+      res.status(200).json({
+        isSuccess: true,
+        data: trip,
+      });
+    } else {
+      res.status(404).json({
+        isSuccess: false,
+        msg: "Trip not found",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
       isSuccess: false,
-      msg: "Trip not found",
+      msg: "Failed to get a trip data",
     });
   }
 };
